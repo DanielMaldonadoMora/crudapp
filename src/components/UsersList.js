54d1880908f113ec7cdfb419ff4be3438c7b653f
Daniel Maldonado <daniel_maldonado79@hotmail.com>
2022-03-22 00:02:00 -0600

    Apellido y correo

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -5,7 +5,8 @@ const UsersList = ({users, selectUser,deleteUser}) => {
         <ul>
             {users.map(user=>(
                 <li key={user.id}>
-                     <h4> {user.first_name}</h4>
+                     <h4> {user.first_name} {user.last_name}</h4>
+                     <p>{user.email}</p>
                      <p>{user.birthday}</p>
                      <button onClick={()=>selectUser(user)}>Seleccionar</button>
                      <button onClick={()=>deleteUser(user.id)}>Eliminar</button> 
